bf91fd8ae3dd620e53a631c5ea0f9fe1cc7afe21
liumq <495800763@qq.com>
2021-11-27 15:51:22 +0800

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.ServiceReference1;
+using static System.Console;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "Client";
+            int intParam;
+            do
+            {
+                WriteLine("Enter an integer and press enter to call the WCF service.");
+            } while (!int.TryParse(ReadLine(), out intParam));
+            Service1Client client = new Service1Client();
+            WriteLine(client.GetData(intParam));
+            WriteLine("Press an key to exit.");
+            ReadKey();
+        }
+    }
+}
